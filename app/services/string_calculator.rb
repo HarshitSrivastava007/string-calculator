88f9be84c57b3823359ebc4add2a78ed2fc3e12e
Harshit <harshit.s@quickinsure.co.in>
2025-07-16 19:30:20 +0530

    add case to supports custom delimiters

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -3,6 +3,13 @@ class StringCalculator
     return 0 if numbers.empty?
 
     delimiters = [",", "\n"]
+    
+    if (match = numbers.match(%r{\A//(.+)\n}))
+      delimiter = match[1]
+      numbers = numbers.split("\n", 2).last
+      delimiters = [delimiter]
+    end
+    
     numbers.split(Regexp.union(delimiters)).map(&:to_i).sum
   end
 end
