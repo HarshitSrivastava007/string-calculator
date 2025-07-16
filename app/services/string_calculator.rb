5850c4dfb3ee5233e0222ab692f5d5356dfbad90
Harshit <harshit.s@quickinsure.co.in>
2025-07-16 19:33:04 +0530

    add case to raises exception for negative numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -9,7 +9,17 @@ class StringCalculator
       numbers = numbers.split("\n", 2).last
       delimiters = [delimiter]
     end
-    
-    numbers.split(Regexp.union(delimiters)).map(&:to_i).sum
+
+    nums = numbers.split(Regexp.union(delimiters)).map(&:to_i)
+
+    validate_numbers(nums)
+
+    nums.sum
   end
+
+  private
+    def validate_numbers(numbers)
+      negatives = numbers.select { |num| num < 0 }
+      raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+    end
 end
